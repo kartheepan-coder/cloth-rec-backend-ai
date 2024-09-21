cde413ded0678120d3c6742c0959e9cb04ac89bf
Kartheepan <karthi.thilax@lyfcard.io>
2024-09-21 15:39:36 +0530

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,9 @@ from firebase_admin import credentials
 # firebase_credentials = f"C:\Users\hp\OneDrive\Desktop\final project program\ai-project-backend-main\fashion-ai-cc420-firebase-adminsdk-c8f4s-fd131ade1f.json"
 
 # cred = credentials.Certificate(firebase_credentials)
-# firebase_admin.initialize_app(cred)
-firebase_cred = os.path.join(os.getcwd(),'firebase_auth.json')
-print(firebase_cred)
+# firebase_admin.initialize_app(credp)
+
+firebase_cred = os.path.join(os.getcwd(),"app",'firebase_auth.json')
 cred = credentials.Certificate(firebase_cred)
 firebase_admin.initialize_app(cred)
 
@@ -53,7 +53,6 @@ def create_app():
     DRESS_PATH = os.path.join(app.config['SEGMENTED_IMGS'],'dress')
     SKIN_FOLDER = os.path.join(app.config['SEGMENTED_IMGS'], 'skin')
 
-
     # @app.after_request
     # def add_cors_headers(response):
     #     response.headers.add('Access-Control-Allow-Origin', '*')  # Allow all origins, or specify a specific one like 'http://localhost:3000'
@@ -65,6 +64,7 @@ def create_app():
     def setup():
         g.csv_helper = CSVHelper(app.config['CSV_FILE'])
         g.csv_helper.initialize_csv()
+       
 
     # Ensure the upload folder exists
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from flask import Blueprint, request, jsonify,g, send_file, send_from_directory
 import joblib
@@ -24,17 +25,14 @@ def checkName():
     data = request.get_json()
     user_name = data['name']
     print(user_name)
-    
     response = g.csv_helper.read_row(user_name)
     return response
-    # g.csv_helper()
 
 
 @api_blueprint.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'OPTIONS':
         return '', 204
-     
     user_name = request.form.get("name")
     user_image = request.files.get("file")
     rensponse = handle_upload(name=user_name,image=user_image)
@@ -44,24 +42,9 @@ def upload():
 
 @api_blueprint.route('/segment',methods=['POST'])
 def segment():
-    # print(request.json)
     data = request.get_json()
     filename = data['filename']
-    # dress = data['is_dress']
-    # print(dress)
-    
-   
     segmenter(filename)
-    # if data['is_dress']:
-    #     path = segmenter(filename=filename)
-    #     print(path)
-    # return path
-
-    
-    
-
-
-    # segmenter()
     if not filename:
         return jsonify({"error": "Filename not provided"}), 400
 
@@ -74,10 +57,6 @@ def segment():
 def get_image(name):
     from app import create_app
     app = create_app()
-    # Send the file from the IMAGE_FOLDER directory
-    # param1 = request.args.get('param1')
-    # return send_from_directory( app.config['UPLOAD_FOLDER'], filename)
-
     img_response = g.csv_helper.image_url(name=name)
     print(img_response)
     return send_file(img_response[0], mimetype='image/jpeg')
@@ -85,7 +64,7 @@ def get_image(name):
 
 
 
-BASE_IMAGE_PATH= os.path.join(os.getcwd(),'Clothes')
+BASE_IMAGE_PATH= os.path.join(os.getcwd(),"app",'Clothes')
 @api_blueprint.route('/get-cloth-image', methods=['GET'])
 def get_cloth_image():
     try:
@@ -185,6 +164,23 @@ def dbTest():
     
 
 
+def get_user_by_id(user_id):
+    try:
+        db = firestore.client()
+        # Reference the user's document by ID
+        user_ref = db.collection('users').document(user_id)
+        user_doc = user_ref.get()
+
+        if user_doc.exists:
+            user =  user_doc.to_dict()
+            user['user_id'] = user_id
+            return user
+        else:
+            return None  # User document does not exist
+    except Exception as e:
+        print(f"Error retrieving user: {e}")
+        return None
+
 
 
 @api_blueprint.route('/signup', methods=['POST'])
@@ -209,12 +205,42 @@ def sign_up():
             'phone': phone,
             'password': hashed_password.decode('utf-8')
         }
-        user = db.collection('users').document().set(user_data)
+         # Create a document with an auto-generated ID
+        user_ref = db.collection('users').add(user_data)
 
-        return jsonify({'message': 'User created successfully '}), 201
+        # Extract the document ID
+        user_id = user_ref[1].id
+        user_info = get_user_by_id(user_id)
+        # Return the document ID in the response
+        return jsonify({'message': 'User created successfully', 'user_info': user_info}), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     # return jsonify("data")
+
+@api_blueprint.route('/update_user', methods=['POST'])
+def updateUser():
+    db=firestore.client()
+    data = request.get_json()
+    user_id = data['user_id']
+    user_gender = data['userGender']
+    user_tone = data['faceTone']
+    user_img_src = data['filename']
+   
+    user_ref = db.collection('users').document(user_id)
+    user_ref.set({
+        "gender":user_gender,
+        "userTone":user_tone,
+        'imageSrc':user_img_src
+    }, merge=True)
+    read = user_ref.get()
+    print(read.to_dict())
+
+   
+    return jsonify(read.to_dict()),200
+   
+
+
+
 
 @api_blueprint.route('/login', methods=['POST'])
 def login():
@@ -224,186 +250,55 @@ def login():
     password = data.get('password')
 
     if not name or not password:
-        return jsonify({'error': 'name and password are required'}), 400
+        return jsonify({'error': 'Name and password are required'}), 400
 
     try:
-        # Find the user in Firestore by phone number
-        user_ref = db.collection('users').where('name' ,"==", name).get()
+        # Find the user in Firestore by name
+        user_ref = db.collection('users').where('name', "==", name).get()
         
         if len(user_ref) == 0:
             return jsonify({'error': 'User not found'}), 404
 
         user_data = [doc.to_dict() for doc in user_ref]
-        # print(user_data)
-
+        
         # Verify the password
         stored_password_hash = user_data[0]['password']
         if not bcrypt.checkpw(password.encode('utf-8'), stored_password_hash.encode('utf-8')):
             return jsonify({'error': 'Invalid credentials'}), 401
 
-        return jsonify(user_data), 200
+        # Include user_id in the response
+        response_data = {
+            "user_id": user_ref[0].id,  # Get the document ID (user_id)
+            **user_data[0]               # Include the user data
+        }
+
+        return jsonify({'message': 'User Logged in successfully', 'user_info': response_data}), 200
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
-
-
-# Initialize data
-female_data = {
-    'user_id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    'skin_tone_r': [255, 204, 139, 255, 180, 120, 230, 160, 100, 220],
-    'skin_tone_g': [224, 153, 69, 185, 150, 90, 200, 130, 70, 190],
-    'skin_tone_b': [189, 102, 19, 140, 120, 60, 170, 100, 40, 160],
-    'skin_tone_category': ['Light', 'Medium', 'Dark', 'Light', 'Medium', 'Dark', 'Light', 'Medium', 'Dark', 'Light'],
-    'purchased_item_id':[ 
-
-18,
-13,
-19,
-3,
-13,
-19,
-18,
-3,
-28,
-12,
-]}
-
-male_data = {
-    'user_id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    'skin_tone_r': [255, 204, 139, 255, 180, 120, 230, 160, 100, 220],
-    'skin_tone_g': [224, 153, 69, 185, 150, 90, 200, 130, 70, 190],
-    'skin_tone_b': [189, 102, 19, 140, 120, 60, 170, 100, 40, 160],
-    'skin_tone_category': ['Light', 'Medium', 'Dark', 'Light', 'Medium', 'Dark', 'Light', 'Medium', 'Dark', 'Light'],
-    'purchased_item_id': [11,
-2,
-14,
-4,
-11,
-12,
-4,
-2,
-3,
-11,
-]}
-
-
-
-# Flask endpoint to get recommendations for a new user
-@api_blueprint.route('/recommend', methods=['POST'])
-def recommend():
-    info = request.get_json()
-    gender = info['gender']
-    colorTone = info['colorTone']
-        # Create DataFrame
-
-    if gender == "male":
-        df = pd.DataFrame(male_data)
-    else:
-        df = pd.DataFrame(female_data)
-
-    # Convert categorical features to numerical
-    label_encoder = LabelEncoder()
-    df['skin_tone_category'] = label_encoder.fit_transform(df['skin_tone_category'])
-
-    # Extract features for clustering
-    features = df[['skin_tone_r', 'skin_tone_g', 'skin_tone_b', 'skin_tone_category', 'purchased_item_id']]
-
-    # Normalize features
-    scaler = StandardScaler()
-    features_normalized = scaler.fit_transform(features)
-
-    # Apply K-means
-    n_clusters = 3
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-    df['cluster'] = kmeans.fit_predict(features_normalized)
-
-    # Function to assign a new user to a cluster
-    def assign_user_to_cluster(user_features, kmeans_model, scaler):
-        user_features_scaled = scaler.transform([user_features])
-        cluster = kmeans_model.predict(user_features_scaled)
-        return cluster[0]
-
-    # Function to recommend items based on the cluster
-    def recommend_items(user_cluster, data):
-        recommended_items = data[data['cluster'] == user_cluster]['purchased_item_id']
-        return recommended_items.unique()
-    
-    def hex_to_rgb(hex):
-        hex_color = hex.lstrip('#')
-        return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-    
-        # Function to calculate luminance
-    def calculate_luminance(rgb):
-        r, g, b = rgb
-        return 0.299*r + 0.587*g + 0.114*b
-
-    # Classify skin tones based on luminance
-    def classify_skin_tone(luminance):
-        if luminance >= 200:
-            return 'Light'
-        elif luminance >= 100:
-            return 'Medium'
-        else:
-            return 'Dark'
-
-    color = hex_to_rgb(colorTone)
-    print(color[0])
-    # Parse JSON request data
-
-    skin_tone_r = color[0]
-    skin_tone_g = color[1]
-    skin_tone_b = color[2]
-    skin_tone_category = classify_skin_tone(calculate_luminance(color))
-    purchased_item_id = male_data['purchased_item_id'][0]
-
-    print(
-    classify_skin_tone(calculate_luminance(color))
-
-    )
-    # # Convert skin tone category to numerical value
-    skin_tone_category_num = label_encoder.transform([skin_tone_category])[0]
-
-    # # Create user feature vector
-    user_features = [skin_tone_r, skin_tone_g, skin_tone_b, skin_tone_category_num, purchased_item_id]
-
-    # # Assign user to a cluster
-    user_cluster = assign_user_to_cluster(user_features, kmeans, scaler)
-
-    # # Get recommendations
-    recommended_items = recommend_items(user_cluster, df)
-    print(recommended_items)
-
-    # Return recommendations as JSON response
-    # return jsonify({
-    #     'user_cluster': user_cluster,
-    #     'recommended_items': recommended_items.tolist()
-    # })
-    # print(jsonify({"user_cluster":user_cluster}))
-
-    return "succeed"
 
 
 @api_blueprint.route('/get-images', methods=['POST'])
 def get_images():
     
     # Parse JSON request data
+
     try:
         info = request.get_json()
         color_category = info.get('color_category')
         gender = info.get('gender')
         dressType = info.get("type")
-        if gender == "male":
+        if gender.lower() == "male":
             if dressType == "top":
-                csv_file_path = os.path.join(os.getcwd(),'dressData_male.csv')
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressData_male.csv')
             else:
-                csv_file_path = os.path.join(os.getcwd(),'dressDataBottom_male.csv')
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressDataBottom_male.csv')
         else:
             if dressType == "top":
-                csv_file_path = os.path.join(os.getcwd(),'dressData_female.csv')
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressData_female.csv')
                 print("top female")
             else:
-                csv_file_path = os.path.join(os.getcwd(),'dressDataBottom_female.csv')
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressDataBottom_female.csv')
                 print("bottom female")
 
             
@@ -505,19 +400,24 @@ def get_images_by_id():
 
 
 # Load the pre-trained models
-modelPath = os.path.join(os.getcwd(),'models')
+modelPath = os.path.join(os.getcwd(),"app",'models')
 
-kmeans = joblib.load(os.path.join(modelPath,"kmeans_model.pkl"))
-scaler = joblib.load(os.path.join(modelPath,"scaler.pkl"))
-preprocessor = joblib.load(os.path.join(modelPath,"preprocessor.pkl"))
+
+# Load the pre-trained models and data
+kmeans = joblib.load(os.path.join(modelPath, "kmeans_model.pkl"))
+scaler = joblib.load(os.path.join(modelPath, "scaler.pkl"))
+preprocessor = joblib.load(os.path.join(modelPath, "preprocessor.pkl"))
+
+
 
 def hex_to_rgb(hex):
     """Convert hex color to RGB tuple."""
     hex_color = hex.lstrip('#')
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
-@api_blueprint.route("/new_recommend", methods=["POST"])
-def recommender():
+
+@api_blueprint.route("/recommend", methods=["POST"])
+def recommendation():
     try:
         # Parse request data
         info = request.get_json()
@@ -525,12 +425,8 @@ def recommender():
         colorTone = info.get('colorTone')
         dressType = info.get("dressType")
 
-        dressCategory = ""
-        if dressType == "top":
-            dressCategory = 'purchased_top'
-        else:
-            dressCategory = 'purchased_bottom'
-        
+
+
         if not gender or not colorTone:
             return jsonify({'error': 'Gender and colorTone are required'}), 400
 
@@ -539,7 +435,9 @@ def recommender():
             'skin_tone_r': hex_to_rgb(colorTone)[0],
             'skin_tone_g': hex_to_rgb(colorTone)[1],
             'skin_tone_b': hex_to_rgb(colorTone)[2],
-            'gender': gender
+            'gender': gender,
+            'purchased_top':12,
+            "purchased_bottom":12
         }])
 
         # Apply transformations
@@ -549,39 +447,88 @@ def recommender():
         # Assign user to a cluster
         user_cluster = kmeans.predict(user_features_scaled)[0]
 
+
         # Load the actual data
-        # Replace the path with your actual file paths
-        actual_data_path = 'app/user_data_with_rgb.csv'
+        actual_data_path = 'app/datas/user_data_with_rgb.csv'
         df = pd.read_csv(actual_data_path)
-        
+
+
         # Ensure the DataFrame contains necessary columns
-        required_columns = ['skin_tone_r', 'skin_tone_g', 'skin_tone_b', 'gender', dressCategory]
-        for col in required_columns:
-            if col not in df.columns:
-                return jsonify({'error': f'Missing column: {col}'}), 500
+        required_columns = ['skin_tone_r', 'skin_tone_g', 'skin_tone_b', 'gender', 'purchased_top', 'purchased_bottom', 'cluster']
+        missing_columns = [col for col in required_columns if col not in df.columns]
+        if missing_columns:
+            return jsonify({'error': f'Missing columns: {missing_columns}'}), 500
         
         # Apply preprocessing to the actual data
         df['gender'] = df['gender'].astype(str)  # Ensure gender is in the same format
-        df_features = preprocessor.transform(df[['skin_tone_r', 'skin_tone_g', 'skin_tone_b', 'gender']])
+        df_features = preprocessor.transform(df[['skin_tone_r', 'skin_tone_g', 'skin_tone_b', 'gender','purchased_top', 'purchased_bottom',]])
         df_scaled = scaler.transform(df_features)
         df['cluster'] = kmeans.predict(df_scaled)
 
-        # Get recommendations based on the cluster
-        recommended_items = df[df['cluster'] == user_cluster]['purchased_top'].unique()
-        print({
-            'user_cluster': user_cluster,
-            'recommended_items': recommended_items.tolist()
-        })
-        return jsonify({"recommended_items":recommended_items.tolist()})
-    
+       # Get recommendations based on the cluster
+        if dressType == "top":
+            recommended_items = df[df['cluster'] == user_cluster]['purchased_top'].unique()
+        else:
+            recommended_items = df[df['cluster'] == user_cluster]['purchased_bottom'].unique()
+
+        # Limit to the first 6 recommendations
+        recommended_items = recommended_items[:6]  # Get the first 6 items
+
+        if gender.lower() == "male":
+            if dressType == "top":
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressData_male.csv')
+                print("top male")
+
+            else:
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressDataBottom_male.csv')
+                print("bottom female")
+
+        else:
+            if dressType == "top":
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressData_female.csv')
+                print("top female")
+            else:
+                csv_file_path = os.path.join(os.getcwd(),"app",'dressDataBottom_female.csv')
+                print("bottom female")
+
+        df = pd.read_csv(csv_file_path)
+        filtered_df = df[df['id'].isin(recommended_items)]
+
+        
+
+        if filtered_df.empty:
+            return jsonify({'error': 'No images found for the specified color category'}), 404
+
+                # Return image names and prominent colors
+        result = filtered_df[['id','image_name', 'prominent_color']].to_dict(orient='records')
+
+
+
+        #  Helper function to convert RGB string to hex
+        def rgb_to_hex(rgb_str):
+            # Convert "(255, 0, 0)" -> "#FF0000"
+            rgb = tuple(map(int, rgb_str.strip("()").split(",")))
+            return '#{:02x}{:02x}{:02x}'.format(rgb[0], rgb[1], rgb[2]).upper()
+
+        # Convert RGB values to hex and prepare the result
+        result = []
+        for _, row in filtered_df.iterrows():
+            hex_color = rgb_to_hex(row['prominent_color'])
+            result.append({
+                'id':row['id'],
+                'image_name': row['image_name'],
+                'prominent_color': hex_color
+            })
+
+        return jsonify(result), 200
 
     except Exception as e:
+        print("error:",e)
         return jsonify({'error': str(e)}), 500
 
 
 
-
-
+ 
 @api_blueprint.route("/save_purchase", methods=['POST'])
 def purchase():
     try:
@@ -589,19 +536,34 @@ def purchase():
         db = firestore.client()
         purchased_top = info.get('purchasedTop')
         purchased_bottom = info.get('purchasedBottom')
-        skin_tone = info.get('skinTone')
+        user_id = info.get('user_id')
 
-        if purchased_top is None or purchased_bottom is None or skin_tone is None:
+
+        if purchased_top is None or purchased_bottom  is None or user_id is None:
             return jsonify({'error': 'Missing required fields'}), 400
+        
+        user_ref = db.collection('users').document(user_id)
+        # Reference to the purchase sub-collection in the user's document
 
-        # Create a unique document ID for each purchase entry
-        purchase_ref = db.collection('purchases').add({
+        #  Add purchase data with current timestamp
+        purchase_data = {
             'purchased_top': purchased_top,
             'purchased_bottom': purchased_bottom,
-            'skin_tone': skin_tone
-        })
+            'user_id':user_id
+        }
+        purchase_data['date'] = firestore.SERVER_TIMESTAMP
 
-        return jsonify({'message': 'Purchase saved successfully'}), 200
+        # Create a unique document ID for each purchase entry
+        purchase_ref = user_ref.collection('purchases').add(
+            purchase_data
+        )
+
+        return jsonify({"messgage":"Purchase is added to the user"}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+
+
+    
